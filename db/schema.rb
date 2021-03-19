ff4212361bb113598aea97300e146a8f452de68c
Max Metcalf <maxmetcalf12@gmail.com>
2021-03-19 13:37:31 -0700

    Added links to website

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_192907) do
+ActiveRecord::Schema.define(version: 2021_03_19_203652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2021_03_19_192907) do
     t.boolean "active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "github_link"
+    t.string "backend_stack"
+    t.string "frontend_stack"
+    t.string "other_tech"
   end
 
 end
--- spec/factories/websites.rb
+++ spec/factories/websites.rb
@@ -2,12 +2,16 @@
 #
 # Table name: websites
 #
-#  id         :bigint           not null, primary key
-#  name       :string
-#  url        :string
-#  active     :boolean
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id             :bigint           not null, primary key
+#  name           :string
+#  url            :string
+#  active         :boolean
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  github_link    :string
+#  backend_stack  :string
+#  frontend_stack :string
+#  other_tech     :string
 #
 FactoryBot.define do
   factory :website do
